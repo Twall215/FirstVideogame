19c2bf49df3b576590408f0c5be569d5f13505b3
Trey Wallace <contacttreyw@gmail.com>
2024-11-28 10:15:34 -0600

    Adding speed adjustments

--- ship.py
+++ ship.py
@@ -3,6 +3,7 @@ class Ship:
     #Ship of Theseus
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('spaceship.bmp')
@@ -11,16 +12,23 @@ class Ship:
         #Starts ship in the center
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a decimal value for the ships horizontal position
+        self.x = float(self.rect.x)
+
+
         #movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         #Update the ships pos based on the movement flag
+        #Update the ships x value, not the rect
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.rect.x += self.settings.ship_speed
+        #Update rect object from self.x
+        self.rect.x - self.x
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
